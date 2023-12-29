4ec9329e3a29b90a37e2ef46c8d8c7558b378026
pankaj ram pawar <pankajpawars123@gmail.com>
2023-12-29 23:13:42 +0530

    login route added

--- server/msg/index.js
+++ server/msg/index.js
@@ -11,7 +11,6 @@ app.use(bodyparser.json());
 connectDb();
 
 app.post('/signup', async (req, res) => {
-
     try {
         const newUser = req.body;
         console.log("here is a new user: ", newUser)
@@ -33,6 +32,30 @@ app.post('/signup', async (req, res) => {
     } catch (error) {
         console.error("Error creating the user", error);
         res.status(500).json({error: "internal server error"})
+    }
+})
+
+app.post('/login', async(req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await userModel.findOne({ username });
+
+        if(!user) {
+            console.log('user not found');
+            res.status(401).json({ message: "invalid username" });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (passwordMatch) {
+            res.status(200).json({ message: "user login successfull."});
+        } else {
+            res.status(401).json({ message: "password does not match." });
+        }
+    } catch (error) {
+        console.log("error during login", error);
+        res.status(500).json({ error: "internal server error please try again later."})
     }
 })
 
