1433b678aa4201ca458844557135b09741689bf5
pankaj ram pawar <pankajpawars123@gmail.com>
2023-12-31 07:11:06 +0530

    updated retweets in userSchema

--- server/msg/models/User.js
+++ server/msg/models/User.js
@@ -32,6 +32,10 @@ const UserSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Tweet'
     }],
+    retweets: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Tweet'
+    }],
     followers: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
