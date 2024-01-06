2171beaedaf1d9fdeeae7e314368f98d82ec9a64
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-06 11:18:20 +0530

    ui's added to the layout

--- client/app/home/layout.tsx
+++ client/app/home/layout.tsx
@@ -1,11 +1,25 @@
 import SideNav from "../ui/home/sidenav"
 import TopNavigation from "../ui/home/topNavigation"
-import exploreTab from "../ui/home/exploreTab"
+import ExploreTab from "../ui/home/exploreTab"
+import { Children } from "react"
 
-export default function Layout() {
+export default function Layout({ children } : {children: React.ReactNode}) {
     return (
-        <div>
-
-        </div>
+        <main>
+            <section>
+                <SideNav/>
+            </section>
+            <section>
+                <div>
+                    <TopNavigation/>
+                </div>
+                <div>
+                    {children}
+                </div>
+            </section>
+            <section>
+                <ExploreTab/>
+            </section>
+        </main>
     )
 }
