3e0979e9961cf5d064eabd5e417fd2ad2dd63c8d
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-06 11:02:29 +0530

    created required frontend components file

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <main >
-      this is the twitter page
+      these is a twitter clone made by pankaj ram pawar
     </main>
   )
 }
