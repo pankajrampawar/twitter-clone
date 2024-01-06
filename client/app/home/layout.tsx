7bd4624c9c45004d40bc13072cbaa5afb3514a56
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-06 11:11:57 +0530

    home page and layout page added to /Home route

---
+++ client/app/home/layout.tsx
@@ -0,0 +1,11 @@
+import SideNav from "../ui/home/sidenav"
+import TopNavigation from "../ui/home/topNavigation"
+import exploreTab from "../ui/home/exploreTab"
+
+export default function Layout() {
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ client/app/home/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            this is content of home page
+        </div>
+    )
+}
