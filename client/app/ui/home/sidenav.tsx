30c6a932d3f539417f3b182df7bac810691a8c91
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-07 06:57:03 +0530

    created iconcardprops

--- client/app/ui/home/sidenav.tsx
+++ client/app/ui/home/sidenav.tsx
@@ -1,9 +1,28 @@
 'use client'
+
 import React from 'react';
+
+interface IconCardProps {
+    location: string;
+    name: string;
+}
+
+const IconCard: React.FC<IconCardProps> = ({name, location})=>{
+    return (
+        <div>
+            <div>
+                <img src={require(location)} alt={name} /> 
+                {/* userd require to get the image through next js image */}
+            </div>
+            <div>{name}</div>
+        </div>
+    )
+}
+
 export default function SideNav() {
     return (
         <div>
-            sidenav
+            <IconCard location="/icons/home.svg" name="home" />
         </div>
     )
 }
